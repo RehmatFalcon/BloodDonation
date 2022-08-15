1594c2a01d5eda82bab92f439e6956c5f370065c
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-15 22:37:24 +0545

    feat: users

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using BloodDonation.Models;
+using BloodDonation.Provider.Interfaces;
 using BloodDonation.ViewModels;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloodDonation.Controllers;
@@ -7,14 +10,18 @@ namespace BloodDonation.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IDbConnectionProvider _connectionProvider;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IDbConnectionProvider connectionProvider)
     {
         _logger = logger;
+        _connectionProvider = connectionProvider;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        await using var conn = _connectionProvider.GetConnection();
+        var users = await conn.QueryAsync<User>("SELECT * FROM user");
         return View();
     }
 
