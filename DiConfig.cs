905110189b93a6141d8402ef417b657a881ec0ca
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-15 20:50:40 +0545

    feat: add configuration

---
+++ DiConfig.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonation.Provider;
+using BloodDonation.Provider.Interfaces;
+
+namespace BloodDonation;
+
+public static class DiConfig
+{
+    public static IServiceCollection UseBloodDonation(this IServiceCollection services)
+    {
+        services.AddScoped<IDbConnectionProvider, DbConnectionProvider>();
+        return services;
+    }
+}
