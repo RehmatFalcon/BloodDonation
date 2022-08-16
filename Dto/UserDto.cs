eefe69e4fc1631caf2e0f1922916f4d87cf73eeb
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-16 07:27:49 +0545

    feat: add user dto

---
+++ Dto/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BloodDonation.Dto;
+
+public class UserDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string UserLevel { get; set; }
+}
