50375f5acbe85cc9c5ec50af2839c0a7ae2db253
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-16 07:28:06 +0545

    feat: add user repo

--- DiConfig.cs
+++ DiConfig.cs
@@ -1,5 +1,12 @@
-﻿using BloodDonation.Provider;
+﻿using BloodDonation.Crypter.Interfaces;
+using BloodDonation.Provider;
 using BloodDonation.Provider.Interfaces;
+using BloodDonation.Repository;
+using BloodDonation.Repository.Interfaces;
+using BloodDonation.Seeder;
+using BloodDonation.Seeder.Interfaces;
+using BloodDonation.Services;
+using BloodDonation.Services.Interfaces;
 
 namespace BloodDonation;
 
@@ -7,7 +14,26 @@ public static class DiConfig
 {
     public static IServiceCollection UseBloodDonation(this IServiceCollection services)
     {
-        services.AddScoped<IDbConnectionProvider, DbConnectionProvider>();
+        UseRepo(services);
+        UseServices(services);
+        UseMisc(services);
         return services;
+    }
+
+    private static void UseRepo(IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+
+    private static void UseServices(IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+    }
+
+    private static void UseMisc(IServiceCollection services)
+    {
+        services.AddScoped<IDbConnectionProvider, DbConnectionProvider>()
+            .AddScoped<ICrypter, Crypter.Crypter>()
+            .AddScoped<IUserSeeder, UserSeeder>();
     }
 }
