a6fe92fe1bd732a6c9bb590b01bdf3be21c23357
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-17 08:05:52 +0545

    feat: add validation

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,6 +3,7 @@ using BloodDonation.Crypter.Interfaces;
 using BloodDonation.Dto;
 using BloodDonation.Models;
 using BloodDonation.Provider.Interfaces;
+using BloodDonation.Repository.Interfaces;
 using BloodDonation.Services.Interfaces;
 using Dapper;
 
@@ -12,17 +13,20 @@ public class UserService : IUserService
 {
     private readonly IDbConnectionProvider _dbConnectionProvider;
     private readonly ICrypter _crypter;
+    private readonly IUserRepository _userRepo;
 
-    public UserService(IDbConnectionProvider dbConnectionProvider, ICrypter crypter)
+    public UserService(IDbConnectionProvider dbConnectionProvider, ICrypter crypter, IUserRepository userRepo)
     {
         _dbConnectionProvider = dbConnectionProvider;
         _crypter = crypter;
+        _userRepo = userRepo;
     }
 
     public async Task<User> Create(UserDto userDto)
     {
         using var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
         await using var conn = _dbConnectionProvider.GetConnection();
+        await Validate(userDto.UserName);
         var user = new User()
         {
             UserName = userDto.UserName,
@@ -37,5 +41,13 @@ select LAST_INSERT_ID();
         ", user);
         tx.Complete();
         return user;
+    }
+
+    private async Task Validate(string userName)
+    {
+        if (await _userRepo.GetByUserName(userName) != null)
+        {
+            throw new Exception($"Username `{userName}` already taken. Please use another username");
+        }
     }
 }
